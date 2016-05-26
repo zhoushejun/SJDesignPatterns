b50e8c3215cdb91d0a3399907f71d33cf47c0c8a
zhoushejun <965678322Zhou>
2016-05-26 21:32:48 +0800

    项目注释

--- SJDesignPatterns/AppDelegate.h
+++ SJDesignPatterns/AppDelegate.h
@@ -6,6 +6,13 @@
 //  Copyright (c) 2016年 shejun.zhou. All rights reserved.
 //
 
+/**
+ @file          AppDelegate.h
+ @brief         iOS设计模式
+ @author		shejun.zhou
+ @version		1.0 16-5-26  Creation
+ 
+ */
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
